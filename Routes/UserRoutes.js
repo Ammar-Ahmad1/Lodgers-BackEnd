31c268a6a22596402d45bfdb021b4c31a7950158
Ammar-Ahmad1 <i190678@nu.edu.pk>
2023-01-24 19:01:02 +0500

    reviews

--- Routes/UserRoutes.js
+++ Routes/UserRoutes.js
@@ -1,7 +1,8 @@
-const {registerUser, updateUser} = require('../Controllers/UserController');
+const {registerUser, updateUser, getUserById} = require('../Controllers/UserController');
 const router = require('express').Router();
 router.post('/register-user', registerUser);
 router.put('/update-user/:id', updateUser);
+router.get('/get-user/:id', getUserById);
 
 
 module.exports = router;
