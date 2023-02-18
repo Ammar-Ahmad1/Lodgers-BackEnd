a72654ba338bfc16d66a2169ce7b0c6caa22e581
Ammar-Ahmad1 <i190678@nu.edu.pk>
2023-02-18 23:19:27 +0500

    reset password complete with OTP

--- Routes/UserRoutes.js
+++ Routes/UserRoutes.js
@@ -1,10 +1,10 @@
-const {registerUser, updateUser, getUserById} = require('../Controllers/UserController');
+const {registerUser, updateUser, getUserById,emailSend,changePassword} = require('../Controllers/UserController');
 const upload = require('../Middlewares/multer')
 const router = require('express').Router();
 
 router.post('/register-user',upload.single("image") , registerUser);
 router.put('/update-user/:id', updateUser);
 router.get('/get-user/:id', getUserById);
-
-
+router.post("/email-send",emailSend)
+router.post('/change-password', changePassword);
 module.exports = router;
