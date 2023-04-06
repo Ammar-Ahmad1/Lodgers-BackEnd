f2cdda55d0fcd04e75f109d61185ad1690125b7e
Ammar-Ahmad1 <i190678@nu.edu.pk>
2023-04-06 22:15:59 +0500

    sentiment analysis completed

--- Controllers/ReviewController.js
+++ Controllers/ReviewController.js
@@ -1,11 +1,51 @@
 const Review = require("../Models/review");
+const HostelModel = require('../Models/HostelModel');
 const controllerError = require("../utils/controllerError");
+const axios = require("axios");
+
+const getRating = async (review,hostelId) => {
+    await axios.post(`http://localhost:4000/predict`,
+        //add review to the body
+        {
+            sentence: review
+        }
+
+
+    ).then((res) => {
+        
+        //update hostel rating according to the sentiment score recieved which is betwen 0 and 1
+        const score = res.data.sentiment_score;
+        console.log(score);
+        const adjustment = score - 0.5;  // assume that a score of 0.5 is neutral
+        const initial_rating = 3;
+        let new_rating = initial_rating + adjustment;
+        if(new_rating>5){
+            new_rating=5;
+        }
+        if(new_rating<0){
+            new_rating=0;
+        }
+        //upto 1 decimal place
+        new_rating = Math.round(new_rating * 10) / 10;
+        
+        HostelModel.findByIdAndUpdate(hostelId, { $set: { ratings: new_rating } }, { new: true }, (err, doc) => {
+            if (err) {
+                console.log("Something wrong when updating data!");
+            }
+            console.log(doc);
+        }
+        );
+    }).catch((err) => {
+        console.log(err);
+    });
+};
 
 module.exports.addReview = async (req, res, next) => {
 
     try {
         console.log(req.body);
         const { review, user, hostel,name, email } = req.body;
+        getRating(review,hostel);
         const newReview = new Review({
             review,
             user,
